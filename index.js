db30d347e4f6fad60676eccf15636031f639017d
ds3010 <daniel_seijas3010@hotmail.com>
2022-09-19 21:41:34 -0400

    first heroku commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var PORT = process.env.PORT || 5000;
+
 const express = require("express");
 const cors = require("cors");
 
@@ -8,4 +10,4 @@ app.get("/", (req, res) => {
   res.download("./resume.pdf");
 });
 
-app.listen(4000);
+app.listen(PORT);
